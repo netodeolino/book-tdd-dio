0dada46be7cdf6f5873bb82ba2ba9750dc3e2af3
Neto Deolino <netodeolino@gmail.com>
2021-02-25 18:52:22 -0300

    first commit - init project

---
+++ src/main/java/com/br/booktdddio/BookTddDioApplication.java
@@ -0,0 +1,13 @@
+package com.br.booktdddio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTddDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookTddDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/booktdddio/BookTddDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.booktdddio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTddDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
